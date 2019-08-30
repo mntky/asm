3044d27c66fdc0eb12c2bdc58e2441f7cc997bc5
mntky <djminatoo@gmail.com>
2019-08-30 17:58:39 +0900

    ls途中

---
+++ ls/ls.asm
@@ -0,0 +1,15 @@
+section  .text
+	global _start
+
+_start:
+	xor rdx, rdx
+	push rdx
+	mov rax, 0x736c2f2f6e69622f
+	push rax
+	mov rdi, rsp
+
+	; generate argv
+	push rdx
+	mov rbx, 0x6c6c617473696c2d
+	push rbx
+	mov rsi, rsp
